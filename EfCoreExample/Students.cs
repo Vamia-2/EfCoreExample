24fc165f7d68f565fb26e71b307718aac62a4458
Ivan <rivanv2008@gmail.com>
2025-03-07 17:11:12 +0200

    Add project files.

---
+++ EfCoreExample/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreExample;
+
+public class Students
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+    public int Course {  get; set; }
+    public int GroupId { get; set; }
+}
